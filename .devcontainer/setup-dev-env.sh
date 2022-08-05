4aa8efde8532231acb7cc77665622d114acf245a
aifrak <aifrak@users.noreply.github.com>
2022-08-05 16:08:55 +0000

    chore(vscode): Setup dev environment when devcontainer is ready
    - Fix wording
    - Print success message after installing dependencies

---
+++ .devcontainer/setup-dev-env.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+./run install-deps
+
+cat <<EOF
+
+----------------------------------------------------------------
+
+Most documentation is listed under the 'docs' directory.
+
+To see the full list of available bash commands, run this command:
+./run help
+
+----------------------------------------------------------------
+Happy coding! ☺
+
+EOF
