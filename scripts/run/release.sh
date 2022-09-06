7734829d7bf98c6cfdee8f672321a9da959f7177
aifrak <aifrak@users.noreply.github.com>
2022-09-06 16:29:25 +0200

    chore(scripts): Fix fake URL (#549)

--- scripts/run/release.sh
+++ scripts/run/release.sh
@@ -8,7 +8,7 @@ Release commands:
   release:ci [MODIFIER]       Release from CI. If modifier given, it will prerelease.
   release:dry-run             Simulate cutting a release without updating files or pushing to git
   prerelease                  Cut a prerelease, tag with a suffix it and publish it to Github (default: dev)
-  - Usage: prerelease [dev|alpha|beta|rc]
+    - Usage: prerelease [dev|alpha|beta|rc]
   test:release                Dry run release, run tests for release
   test:release:hooks          Run tests for hooks during a release
   test:release-it:custom      Run tests related to custom hooks and/or plugins for release-it
@@ -35,7 +35,7 @@ function release:dry-run {
     --no-git.requireBranch \
     --no-git.requireCleanWorkingDir \
     --no-git.requireUpstream \
-    --git.pushRepo="git://fake.host.for.dry.run:user/repo"
+    --git.pushRepo="git://fake.host.for.dry.run/user/repo"
 }
 
 function test:release {
