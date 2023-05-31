611f34864c3aaa15b49d5c45e95452636a99d7a7
Franklin Rakotomalala <aifrak@users.noreply.github.com>
2023-05-31 19:34:41 +0200

    chore(scripts): Improve help messages (#845)

--- scripts/run/release.sh
+++ scripts/run/release.sh
@@ -5,11 +5,11 @@ function help:release {
 
 Release commands:
   release                   Update CHANGELOG.md and version files, cut a release and publish to Github
-  release:ci                Release from CI. If modifier given, it will prerelease.
-    - Usage: release:ci [MODIFIER]
+  release:ci                Cut a release from CI. If modifier given, it will prerelease.
+    - Usage: release:ci [dev|alpha|beta|rc]
   release:dry-run           Simulate cutting a release without updating files or pushing to git
   prerelease                Cut a prerelease, tag with a suffix it and publish it to Github (default: dev)
-    - Usage: prerelease [dev|alpha|beta|rc]
+    - Usage: prerelease [dev|alpha|beta|rc] (default: dev)
   test:release              Dry run release, run tests for release
   test:release:hooks        Run tests for hooks during a release
   test:release-it:custom    Run tests related to custom hooks and/or plugins for release-it
